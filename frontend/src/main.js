2aef4744cf67cf8a6d577913fc465faaa5acc51c
Alan <alan_berns@yahoo.com.ar>
2023-12-24 13:18:17 -0300

    vue-router + bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 import axios from 'axios'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const app = createApp(App)
+    .use(router)
 
 app.config.globalProperties.axios=axios
 
---
+++ frontend/src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Componentes
+
+
+// definir objeto rutas
+const routes = [
+
+
+];
+
+// crear objeto rutas
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
